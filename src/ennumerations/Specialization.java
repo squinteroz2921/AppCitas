554261a231848fecf881d713d270961b8e74d78e
SebastianQuintero <squinteroz2921@gmail.com>
2020-05-24 20:51:06 -0500

    se crea el indice del la clase Specialization

--- src/ennumerations/Specialization.java
+++ src/ennumerations/Specialization.java
@@ -1,5 +1,13 @@
+
 package ennumerations;
 
 public enum Specialization {
-    BARBA,CORTE,BARBA_CORTE;
+    BARBA(1),CORTE(2),BARBA_CORTE(3);
+    private final int intEnum;
+    Specialization(int intEnum){
+        this.intEnum=intEnum;
+    }
+    public int getIntEnum(){
+        return this.intEnum;
+    }
 }
