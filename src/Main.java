5fc02ef29d57cdd43883f53e194d5123319f97ca
SebastianQuintero <squinteroz2921@gmail.com>
2020-05-25 11:24:26 -0500

    se crean los objetos de la las implementaciones

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 import domains.Barber;
 import ennumerations.Specialization;
+import service.BarberService;
+import service.BarberShopService;
+import service.ClientService;
+import service.MeetService;
+import service.implement.BarberServiceImplement;
+import service.implement.BarbersShopServiceImplement;
+import service.implement.ClientServiceImplement;
+import service.implement.MeetServiceImplement;
 //import service.BarberService;
 //import service.implement.BarberServiceImplement;
 //import service.implement.BarbersShopServiceImplement;
@@ -11,8 +19,14 @@ import java.util.Map.Entry;
 import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args) throws NoSuchFieldException {
+    BarberService barberServiceImplement = new BarberServiceImplement();
+    BarberShopService barberShopServiceImplement = new BarbersShopServiceImplement();
+    ClientService clientServiceImplement = new ClientServiceImplement();
+    MeetService meetServiceImplement= new MeetServiceImplement();
+
+    public static void main(String[] args) {
         //Implementacion de los metodos de la Barberia//////
+        Scanner userInput = new Scanner(System.in);
 //        BarberService barberServiceImplement=new BarberServiceImplement();
 //        barberServiceImplement.createBarber(1l,"Sebastiao","lol",Specialization.BARBA_CORTE);
 //        barberServiceImplement.createBarber(2l,"Danielao","lolazo",Specialization.BARBA_CORTE);
@@ -20,27 +34,25 @@ public class Main {
 //        barberServiceImplement.updateBarber(1l,"Carolao","Lopez",Specialization.BARBA);
 //        barberServiceImplement.showListBarber();
 //        barberServiceImplement.deleteBarber(1l);
-        Barber barber1=new Barber(1l,"A","A",Specialization.BARBA);
-        Barber barber2=new Barber(2l,"B","B",Specialization.BARBA);
-        Barber barber3=new Barber(3l,"C","C",Specialization.BARBA);
+        Barber barber1 = new Barber(1l, "A", "A", Specialization.BARBA);
+        Barber barber2 = new Barber(2l, "B", "B", Specialization.BARBA);
+        Barber barber3 = new Barber(3l, "C", "C", Specialization.BARBA);
 
 
-        HashMap<Long, Barber> mapTest=new HashMap<>();
-        mapTest.put(1l,barber1);
-        mapTest.put(2l,barber2);
-        mapTest.put(3l,barber3);
+        HashMap<Long, Barber> mapTest = new HashMap<>();
+        mapTest.put(1l, barber1);
+        mapTest.put(2l, barber2);
+        mapTest.put(3l, barber3);
         System.out.println(mapTest.keySet());
         System.out.println("///////////////");
         System.out.println(mapTest.entrySet());
-        for (Map.Entry<Long,Barber> entry:mapTest.entrySet()){
-            System.out.println(entry.getKey()+";"+entry.getValue());
-        }
-
-
-
-
+        for (Map.Entry<Long, Barber> entry : mapTest.entrySet()) {
+            System.out.println(entry.getKey() + ";" + entry.getValue());
         }
 
 
     }
+
+
+}
 
